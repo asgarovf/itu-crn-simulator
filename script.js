64bd4cfa9edcb017ef9253fc590f2ceca0557be4
asgarovf <farhad@getwhelp.com>
2021-07-10 21:15:35 +0400

    feat: clearing feature and empty crn checking

--- script.js
+++ script.js
@@ -9,12 +9,40 @@ $(document).ready(() => {
     e.preventDefault();
     let newTime = Date.now();
 
+    const group = $(".form-group");
+
+    let flag = false;
+    for (let i = 1; i <= group.length; i++) {
+      if (
+        document.querySelector(`.form-group:nth-child(${i}) div input`)
+          .value !== ""
+      ) {
+        flag = true;
+      }
+    }
+
+    if (!flag) {
+      alert("Lütfen bir CRN Giriniz.");
+      throw new Error("Empty crn");
+    }
+
     $("#time").html((newTime - time) / 1000);
     $("#reset").removeClass("d-none");
   });
 
   $("#reset").click(() => {
     window.location.reload(false);
+  });
+
+  $("#clear").click((e) => {
+    e.preventDefault();
+
+    const group = $(".form-group");
+
+    for (let i = 1; i <= group.length; i++) {
+      document.querySelector(`.form-group:nth-child(${i}) div input`).value =
+        "";
+    }
   });
 });
 
